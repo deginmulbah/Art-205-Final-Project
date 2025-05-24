0a5de536acb9a0a77d7dcbe4698b5893cda0a7ad
deginmulbah <jenkinsmulbah6@gmail.com>
2025-05-24 16:09:37 +0000

    added about me modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,10 @@ const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
   navItem = document.querySelectorAll(".nav__item"),
   header = document.getElementById("header");
+
+const aboutModel = document.getElementById("about-modal");
 const submitform = document.getElementById("contact-form");
+const heroImage = document.getElementById("hero-img");
 const contactFormModel = document.getElementById("form-modal");
 
 // open and close menu
@@ -30,6 +33,11 @@ document.getElementById("confirm-btn").addEventListener("click", function () {
   contactFormModel.classList.add("hidden");
 });
 
+// show about me model
+heroImage.addEventListener("click", () => {
+  aboutModel.classList.remove("hidden");
+});
+
 // close the menu when the user clicks the nav links
 navItem.forEach((item) => {
   item.addEventListener("click", () => {
@@ -51,32 +59,6 @@ function changeIcon() {
 
 // Testimonial Slide
 
-// const testimonialSlide = new Swiper(".testimonial__wrapper", {
-//   loop: true,
-//   spaceBetween: 30,
-//   centeredSlides: true,
-//   effect: "coverflow",
-//   grabCursor: true,
-//   slidesPerView: 1,
-//   coverflowEffect: {
-//     rotate: 50,
-//     stretch: 0,
-//     depth: 100,
-//     modifier: 1,
-//     slideShadows: true,
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-
-//   breakpoints: {
-//     520: {
-//       slidesPerView: "auto",
-//     },
-//   },
-// });
-
 // header scroll animation
 window.addEventListener("scroll", () => {
   if (window.scrollY > 40) {
@@ -87,29 +69,29 @@ window.addEventListener("scroll", () => {
 });
 
 // ScrollReveal animations
-// const sr = ScrollReveal({
-//   duration: 2000,
-//   distance: "100px",
-//   delay: 400,
-//   reset: false,
-// });
+const sr = ScrollReveal({
+  duration: 2000,
+  distance: "100px",
+  delay: 400,
+  reset: false,
+});
 
-// sr.reveal(".hero__content, .about__content");
-// sr.reveal(".hero__img", { origin: "top" });
+sr.reveal(".hero__content, .about__content");
+sr.reveal(".hero__img", { origin: "top" });
 
-// sr.reveal(
-//   ".hero__info-wrapper, .skills__title, .skills__content, .qualification__name, .qualification__item, .service__card, .project__content, .testimonial__wrapper, .footer__content",
-//   {
-//     delay: 500,
-//     interval: 100,
-//   }
-// );
+sr.reveal(
+  ".hero__info-wrapper, .skills__title, .skills__content, .qualification__name, .qualification__item, .service__card, .project__content, .testimonial__wrapper, .footer__content",
+  {
+    delay: 500,
+    interval: 100,
+  }
+);
 
-// sr.reveal(".qualification__footer-text, .contact__content", {
-//   origin: "left",
-// });
+sr.reveal(".qualification__footer-text, .contact__content", {
+  origin: "left",
+});
 
-// sr.reveal(".qualification__footer .btn, .contact__btn", { origin: "right" });
+sr.reveal(".qualification__footer .btn, .contact__btn", { origin: "right" });
 
 document.addEventListener("DOMContentLoaded", () => {
   const yearSpan = document.querySelector(".footer__copyright");
